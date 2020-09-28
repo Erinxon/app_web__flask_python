7061d7fad85a2517a7fabf324059b89313f02c77
unknown <erinxons@gmail.com>
2020-09-28 16:31:01 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
+from flask import Flask, render_template, request, redirect, url_for, flash
+from urllib.request import urlopen
+import json
+import urllib.request
+import datetime
+
+
+app = Flask(__name__)
+app.secret_key = "mysecretkey"
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/lista', methods=['GET', 'POST'])
+def lista():
+    try:
+        nombre_pokemon = request.form.get("nombre_pokemon")
+        user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+
+        url = "https://pokeapi.co/api/v2/pokemon/" + nombre_pokemon
+              
+        headers = {'User-Agent': user_agent}
+
+        request2 = urllib.request.Request(url, None, headers)
+        response = urllib.request.urlopen(request2)
+        buscar_informacion = json.loads(response.read())
+
+        if nombre_pokemon == buscar_informacion['name']:
+            print("Si")
+            nombre = buscar_informacion['name']
+            nombre = nombre.capitalize()
+            peso = buscar_informacion['weight']
+            img = buscar_informacion['sprites']['front_default']
+            habilidad = buscar_informacion['abilities'][0]['ability']['name']
+            habilidad = habilidad.capitalize()
+            altura = buscar_informacion['height']
+            url_info = buscar_informacion['species']['url']
+
+            tipo = buscar_informacion['types'][0]['type']['name']
+            tipo = tipo.capitalize()
+
+            img = buscar_informacion['sprites']['front_default']     
+
+            return render_template('lista.html', n=nombre, p=peso, h=habilidad, a=altura, t=tipo, i=img)
+        else:
+            return render_template('lista.html', n=None, p=False)
+    except:
+        return render_template('lista.html', n=None, p=False)
+
+
+app.run(debug=True)
