3e11e9d7c1ab3f187b95aceacda796f3ffef738f
unknown <erinxons@gmail.com>
2020-09-28 22:57:22 -0400

    make it better

--- static/app.js
+++ static/app.js
@@ -4,9 +4,11 @@ const botonBuscar = document.querySelector('#buscar');
 if(botonBuscar){
     botonBuscar.addEventListener('click', function(e){
         console.log('Click en buscar pokemon');
+        
         botonBuscar.innerHTML = `<div class="spinner-border text-light" role="status">
         <span class="sr-only">calgando</span>
         </div>`;
+       
         botonBuscar.disabled = false
         e.stopPropagation();
     });
